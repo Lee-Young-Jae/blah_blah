78cd6a707d29189f7ac72d4b190e2680ab19e955
Lee-Young-Jae <yjlee1937@naver.com>
2022-12-21 21:49:27 +0900

    feat: 사용자 정보 조회 기능 추가

---
+++ pages/api/user.info/[screenName].ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import MemberCtrl from '@/controllers/member.ctrl';
+import handleError from '@/controllers/error/handle.error';
+import checkSupportMethod from '@/controllers/error/check_support_method';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  const supportMethods = ['GET'];
+
+  try {
+    checkSupportMethod(supportMethods, method);
+    await MemberCtrl.findByScreenName(req, res);
+  } catch (error) {
+    console.error(error);
+    // 에러 처리
+    handleError(error, res);
+  }
+}
