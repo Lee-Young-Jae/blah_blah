41ec5d20010392179928b7ff007686b5b5ba40fe
Lee-Young-Jae <yjlee1937@naver.com>
2022-12-31 01:26:32 +0900

    design: 레이아웃 수정

--- components/service_layout.tsx
+++ components/service_layout.tsx
@@ -1,19 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Box, BoxProps } from '@chakra-ui/react';
 import Head from 'next/head';
 import React from 'react';
+import GNB from './GNB';
 
 interface Props {
   title: string;
   children: React.ReactNode;
 }
 
-const ServiceLayout = function ({ title = 'blah x2', children }: Props) {
+const ServiceLayout: React.FC<Props & BoxProps> = function ({ title = 'blah x2', children, ...boxProps }) {
   return (
-    <div>
+    <Box {...boxProps}>
       <Head>
         <title>{title}</title>
       </Head>
+      <GNB />
+
       {children}
-    </div>
+    </Box>
   );
 };
 
